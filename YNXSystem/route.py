86457112642a91a3a60278fecbed1787bc4bbc2f
Sandoleathy <3296446003@qq.com>
2024-03-19 19:10:10 +0800

    使用了ajax来处理前端计算请求

--- YNXSystem/route.py
+++ YNXSystem/route.py
@@ -1,7 +1,12 @@
 from YNXSystem import app
-from flask import render_template
+from flask import render_template, Flask, request, jsonify
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['POST','GET'])
+@app.route('/index', methods=['POST','GET'])
 def index():
     return render_template("index.html")
+@app.route('/calculate', methods=['POST'])
+def calculate():
+    print("gooo")
+    data = request.form["date"]
+    return jsonify({"data": data})
