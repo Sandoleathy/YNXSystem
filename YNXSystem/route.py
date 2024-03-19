929335bef47c1928ffe2592afa77f27563a3a61a
Sandoleathy <3296446003@qq.com>
2024-03-19 20:28:28 +0800

    完善了ajax逻辑使其正常运作，引入了ajax包

--- YNXSystem/route.py
+++ YNXSystem/route.py
@@ -7,6 +7,14 @@ def index():
     return render_template("index.html")
 @app.route('/calculate', methods=['POST'])
 def calculate():
-    print("gooo")
     data = request.form["date"]
-    return jsonify({"data": data})
+    timeSet = data.split("-")
+    print(timeSet)
+    year = int(timeSet[0])
+    month = int(timeSet[1])
+    day = int(timeSet[2])
+    return jsonify({
+        "result": day,
+        "month": month,
+        "year": year,
+        })
